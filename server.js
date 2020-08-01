9c00ffc381b2e05e52ef3d3c990205fb029e5a65
Glitch (mountain-cheerful-middle) <none>
2020-08-01 09:08:30 +0000

    ⚽️🍼 Updated with Glitch

--- server.js
+++ server.js
@@ -24,7 +24,43 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.route('/api/timestamp/:date?')
+  .get(function (req, res){
+    
+    var date = null;
+    // parse the date string
+    if (req.params.date !== undefined) {
 
+      // check if it is a unix timestamp...
+      var unixTimestamp = parseInt(req.params.date*1);
+      if (isNaN(unixTimestamp)) {
+        
+        // it's not a unix timestamp string
+        date = new Date(req.params.date);
+      } else {
+        
+        // it is a timestamp
+        date = new Date(unixTimestamp);
+      }
+      
+    } else {
+      
+      // the date string parameter is empty. 
+      // create a new date based on current time 
+      date = new Date(Date.now());
+    }
+    
+    // Initialize the response object, if Date is invalid
+    // this one will be returned
+
+    var response = date == "Invalid Date" ? 
+      { error: "Invalid Date" } :
+      { "unix": date.getTime(),
+        "utc": date.toUTCString()
+      };
+    
+    res.json(response);
+  });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
